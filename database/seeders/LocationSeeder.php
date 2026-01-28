1e708258f929aeeb4242a0ea8e6f963bf9d3017b
JoshuaHD <22371601+JoshuaHD@users.noreply.github.com>
2026-01-28 15:13:31 -0500

    Fixed style

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -19,58 +19,60 @@ class LocationSeeder extends Seeder
 
         $locationsCount = 0;
         foreach ($files as $file) {
-            $path = database_path("seeders/data/${file}" );
+            $path = database_path("seeders/data/{$file}");
             $rows = array_map('str_getcsv', file($path));
             $header = [
-                "id",
-                "name",
-                "city",
-                "country",
-                "iata",
-                "icao",
-                "lat",
-                "lng",
-                "alt",
-                "timezone",
-                "dst",
-                "tz",
-                "type",
-                "source",
+                'id',
+                'name',
+                'city',
+                'country',
+                'iata',
+                'icao',
+                'lat',
+                'lng',
+                'alt',
+                'timezone',
+                'dst',
+                'tz',
+                'type',
+                'source',
             ];
 
             $mappedRows = [];
 
             foreach ($rows as $row) {
-                if (count($row) != count($header))
+                if (count($row) != count($header)) {
                     continue;
+                }
 
                 $row = array_map(function ($col) {
-                    if ($col === '\N')
+                    if ($col === '\N') {
                         return null;
+                    }
 
                     return $col;
                 }, $row);
 
-
-                //dd([$header, $row]);
                 $row = array_combine($header, $row);
 
-                if(!$row['iata'])
-                    $row['iata'] = "Z-" . $row['id'];
+                if (! $row['iata']) {
+                    $row['iata'] = 'Z-'.$row['id'];
+                }
 
                 $mappedRows[] = $row;
             }
 
-            $this->command->info("Seeding " . $file);
+            $this->command->info('Seeding '.$file);
             Location::insertOrIgnore($mappedRows);
             $insertedRowCount = count($mappedRows);
             $locationsCount += $insertedRowCount;
-            $this->command->info("Seeded ${insertedRowCount} rows from ${file}");
+            $this->command->info("Seeded {$insertedRowCount} rows from {$file}");
         }
 
         $savedCount = Location::count();
 
-        if ($savedCount < $locationsCount)
+        if ($savedCount < $locationsCount) {
             $this->command->warn("Expected to have {$locationsCount} but only inserted {$savedCount}");
+        }
     }
 }
