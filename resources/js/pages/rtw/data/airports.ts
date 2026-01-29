14c2afa701e9d45e9f5bd45836f60e6386a1f5ff
JoshuaHD <22371601+JoshuaHD@users.noreply.github.com>
2026-01-29 09:46:42 -0500

    Allow for retrieving filtered airports by ID and IATA

--- resources/js/pages/rtw/data/airports.ts
+++ resources/js/pages/rtw/data/airports.ts
@@ -46,8 +46,24 @@ const allLocations = [...airports, ...customLocations];
 export const byIata = indexBy(allLocations, 'iata', false) as FlatResult;
 export const byId = indexBy(allLocations, 'airportId', false) as FlatResult;
 
-export const getFilteredAirports = (iataCodes: (keyof typeof byIata|string)[]): Airport[] => {
+export const getFilteredAirports = (
+    iataCodes: (keyof typeof byIata | keyof typeof byId | string)[],
+): Airport[] => {
     return iataCodes
-        .map((code) => ({ ...byIata[code as keyof typeof byIata] as object, iata: code }))
+        .map((code) => {
+            code = code.toString();
+
+            if (code.startsWith('Z-')) {
+                return {
+                    ...(byId[code.substring(2) as keyof typeof byId] as object),
+                    iata: code,
+                };
+            } else {
+                return {
+                    ...(byIata[code as keyof typeof byIata] as object),
+                    iata: code,
+                };
+            }
+        })
         .filter((airport) => airport !== undefined);
-}
+};
